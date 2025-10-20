16d5a7742b93ff2b92a7196d27f55a3fef94d64e
ricebread52 <jeneilrisethjohn@gmail.com>
2025-10-20 19:22:22 +0530

    envs added

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:5000/api"; // Adjust for production later
+const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5000/api"; // Adjust for production later
 
 // Signup
 export const signup = async (data: { name: string; dob: string; email: string; password?: string }) => {
